70cc465bc8551704043443ed3ea839ea186af047
Joey <far.field@gmail.com>
2025-11-29 19:01:20 -0700

    endpoints for users complete and tested

--- server/index.js
+++ server/index.js
@@ -1,21 +1,27 @@
-require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
+const app = express();
+require('dotenv').config();
+
 
 const userRoute = require('./routes/users');
 const teamsroute = require('./routes/teams');
 const feedbackRoute = require('./routes/feedback');
 
-const app = express();
 
 //Middleware
 app.use(cors());
 app.use(express.json());
 
 //Routes
-app.use('/api/users', userRoute);
-app.use('/api/teams', teamsroute);
-app.use('/api/feedback', feedbackRoute);
+
+app.use('/users', require('./routes/users'));
+
+app.get('/', (req, res) => res.send('API is live'));
+// app.use('/api/users', userRoute);
+// app.use('/api/teams', teamsroute);
+// app.use('/api/feedback', feedbackRoute);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
--- server/routes/users.js
+++ server/routes/users.js
@@ -2,7 +2,7 @@ const expess = require('express');
 const router = expess.Router();
 const prisma = require('../prisma/client');
 
-//GET all users
+//GET all users and order in ascending order by ID
 router.get('/', async (req, res) => {
     try {
         const users = await prisma.user.findMany({orderBy: {id:'asc'}});
@@ -11,22 +11,58 @@ router.get('/', async (req, res) => {
         res.status(500).json({ error: 'Failed to fetch users' });
     }
 });
-
-//Create a new user
-router.post('/', async (req, res) => {
-    const { name, email, role } = req.body;
-
-    if(!name || !email || !role) {
-        return  res.status(400).json({ error: 'Name, email and role are required' });
-    }   
+//GET user by ID
+router.get('/:id', async (req, res) => {
     try {
-        const user = await prisma.user.create({
-            data: { name, email, role },
+        
+        const user = await prisma.user.findUnique({ where: { id: parseInt(req.params.id) } });
+        if(!user){
+            return  res.status(404).json({ error: 'User not found' });
+        }
+        res.json(user);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch user' });
+    }
+}); 
+
+//POST create new user
+router.post('/', async (req, res) => {
+    const {name, email, role} = req.body;
+    try {
+        const newUser = await prisma.user.create({
+            data: {
+                name,
+                email,
+                role
+            }
         });
-        res.status(201).json(user);
+        res.status(201).json(newUser);
     } catch (error) {
         res.status(500).json({ error: 'Failed to create user' });
     }
 });
+
+//PUT update user by ID
+router.put('/:id', async (req, res) => {
+    const {name, email, role} = req.body;
+    try {
+        const updatedUser = await prisma.user.update({
+            where: { id: parseInt(req.params.id) },
+            data: { email, name, role } });
+        res.json(updatedUser);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to update user' });
+    }
+}); 
+
+//Delete user by ID
+router.delete('/:id', async (req, res) => {
+    try {
+        await prisma.user.delete({ where: { id: parseInt(req.params.id) } });
+        res.status(204).end();
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to delete user' });
+    }
+});     
 
 module.exports = router;
